44803b53d9059884ebfd7e2dcf2a8833c3507143
MuthukumarSazsapps <kumar@sazsapps.com>
2024-10-16 20:46:17 +0530

    user restriction

--- src/app.jsx
+++ src/app.jsx
@@ -5,12 +5,12 @@ import Company from './pages/company/company'
 import Home from './pages/home'
 import NotFoundPage from './pages/404'
 import LoginPage from './pages/auth/login/login'
-import { PrivateRoutesValidator } from './validators/validators'
+import { AdminRoutesValidator, PrivateRoutesValidator, SubscriberRoutesValidator, UserRoutesValidator } from './validators/validators'
 import Customer from './pages/masters/customer/customer'
 import Product from './pages/masters/product/product'
 import VehicleType from './pages/masters/vehicle/vehicleType/vehicleType'
 import Weighing from './pages/weighing/weighing'
-import User from './pages/company/user/user'
+import User from './pages/masters/user/user'
 import CameraView from './pages/camera/CameraView'
 import VehicleNumber from './pages/masters/vehicle/vehicleNumber/vehicleNumber'
 
@@ -23,7 +23,7 @@ const App = () => {
     <>
    
       {/* <Mainlayout/> */}
-      <Routes>
+      {/* <Routes>
         <Route element={<PrivateRoutesValidator />}>
           <Route path='/' element={<Mainlayout />}>
             <Route index element={<Home />} />
@@ -39,7 +39,56 @@ const App = () => {
         <Route path='/login' element={<LoginPage />} />
         <Route path='*' element={<NotFoundPage />} />
 
-      </Routes>
+      </Routes> */}
+
+
+<Routes>
+  {/* Private routes that require authentication */}
+  <Route element={<PrivateRoutesValidator />}>
+    <Route path='/' element={<Mainlayout />}>
+      <Route index element={<Home />} />
+
+      {/* Admin Routes */}
+      
+      {/* Subscriber Routes */}
+      <Route element={<SubscriberRoutesValidator />}>
+   
+        <Route path='o/customer/master' element={<Customer />} /> 
+        <Route path='o/material/master' element={<Product />} />
+        <Route path='o/vehicletype/master' element={<VehicleType />} />
+        <Route path='o/weighing/master' element={<Weighing />} />
+        <Route path='o/user/master' element={<User />} />
+        <Route path='o/vehicleno/master' element={<VehicleNumber />} />
+      </Route>
+
+
+      <Route element={<AdminRoutesValidator />}>
+        <Route path='a/company/create' element={<Company />} />
+        <Route path='a/customer/master' element={<Customer />} />
+        <Route path='a/material/master' element={<Product />} />
+        <Route path='a/vehicletype/master' element={<VehicleType />} />
+        <Route path='a/weighing/master' element={<Weighing />} />
+        <Route path='a/user/master' element={<User />} />
+        <Route path='a/vehicleno/master' element={<VehicleNumber />} />
+      </Route>
+
+      <Route element={<UserRoutesValidator />}>
+        <Route path='u/customer/master' element={<Customer />} /> 
+        <Route path='u/material/master' element={<Product />} />
+        <Route path='u/vehicletype/master' element={<VehicleType />} />
+        <Route path='u/weighing/master' element={<Weighing />} />
+        {/* <Route path='u/users/master' element={<User />} /> */}
+        <Route path='u/vehicleno/master' element={<VehicleNumber />} />
+      </Route>
+
+    </Route>
+  </Route>
+
+  {/* Public Routes */}
+  <Route path='/login' element={<LoginPage />} />
+  <Route path='*' element={<NotFoundPage />} />
+</Routes>
+
     </>
   )
 }
